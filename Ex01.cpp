b4b299bbc06da6610df5e7ce3d6599456f8f6e24
Breno <75366737+Kazees@users.noreply.github.com>
2023-03-16 19:11:04 -0300

    Update Ex01.cpp

--- Ex01.cpp
+++ Ex01.cpp
@@ -9,81 +9,50 @@ using namespace std;
 
 int main()
 {
-    Paciente n1;
     string nome,sexo;
     float peso,altura;
-    bool continuar=true;
+    bool conti=true;
 
-    cout<<"Digite seu nome: ";
-    getline(cin,nome);
-    n1.setNome(nome);
-
-    while(continuar)
+    while(conti)
     {
+        cout<<"Digite seu nome: ";
+        getline(cin,nome);
+
         cout<<"Digite seu sexo: ";
         cin>>sexo;
 
-        try
-        {
-            n1.setSexo(sexo);
-            continuar=false;
-        }
-
-        catch(string erro1)
-        {
-            cout<<erro1<<endl;
-            continuar=true;
-        }
-    }
-
-    continuar=true;
-    cout<<'\n';
-
-    while(continuar)
-    {
         cout<<"Digite seu peso: ";
         cin>>peso;
 
-        try
-        {
-            n1.setPeso(peso);
-            continuar=false;
-        }
-
-        catch(string erro2)
-        {
-            cout<<erro2<<endl;
-            continuar=true;
-        }
-    }
-
-    continuar=true;
-    cout<<'\n';
-
-    while(continuar)
-    {
         cout<<"Digite sua altura: ";
         cin>>altura;
 
         try
         {
-            n1.setAltura(altura);
-            continuar=false;
+            Paciente n1(nome,sexo,peso,altura);
+            cout<<endl<<"Nome: "<<n1.getNome()<<endl;
+            cout<<"Sexo: "<<n1.getSexo()<<endl;
+            cout<<"Peso: "<<n1.getPeso()<<endl;
+            cout<<fixed<<setprecision(2);
+            cout<<"Altura: "<<showpoint<<n1.getAltura()<<endl;
+            cout<<"IMC: "<<n1.calcularIMC()<<endl;
+            conti=false;
         }
 
-        catch(string erro2)
+        catch(invalid_argument &ex)
         {
-            cout<<erro2<<endl;
-            continuar=true;
+            cout<<"Erro: "<<ex.what()<<endl;
+            conti=true;
         }
-    }
 
-    cout<<endl<<"Nome: "<<n1.getNome()<<endl;
-    cout<<"Sexo: "<<n1.getSexo()<<endl;
-    cout<<"Peso: "<<n1.getPeso()<<endl;
-    cout<<fixed<<setprecision(2);
-    cout<<"Altura: "<<showpoint<<n1.getAltura()<<endl;
-    cout<<"IMC: "<<n1.calcularIMC()<<endl;
+        catch(out_of_range &ex)
+        {
+            cout<<"Erro: "<<ex.what()<<endl;
+            conti=true;
+        }
+
+        cin.ignore();
+    }
 
     return 0;
 }
